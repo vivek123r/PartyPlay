d308b17af93a687fe2b3701619083920065f457a
vivek123r <vivekravi9496497657@gmail.com>
2026-07-26 14:58:42 +0530

    fix: show Pages startup state and errors

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from '@platform/App';
 import '@platform/styles/index.css';
 
+document.getElementById('root')?.setAttribute('data-mounted', 'true');
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
